f57d0dcbf20ffd197b3afc62e890479e1a6969ed
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-17 02:45:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(elements)
   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  
 end
 
 def find_the_cheese# code an argument here
