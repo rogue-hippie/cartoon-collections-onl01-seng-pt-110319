dabca97d56fcca51a621a5edccd4e7c6311af4a3
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-17 02:26:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.map.with_index(1) {|name, i| "#{i}. #{name}"}
+  dwarves.map.with_index(1) {|name, i| puts "#{i}. #{name}"}
   
-  dwarves 
 end
 
 def summon_captain_planet# code an argument here
